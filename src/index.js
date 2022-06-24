3f6192f8eeb162d8989765e2db5c3d9f2b156fc1
KirillMolotkov <balbes_goda@mail>
2022-06-24 15:45:35 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const units = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const dozenth = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const hundreds = ["hundred"];
+    const getLastNumber = String(number).split("").pop();
+    const getFirstNumber = String(number).split("").shift();
+    const getSecondNumber = String(number).split("")[1];
+    if (number >= 0 && number <= 19) {
+        return units[number];
+    }
+    if (number >= 20 && number <= 99) {
+        if (getSecondNumber === "0") {
+            return dozenth[getFirstNumber - 2];
+        }
+        return `${dozenth[getFirstNumber - 2]} ${units[getLastNumber]}`;
+    }
+    if (number >= 100 && number <= 999) {
+        if (getLastNumber === "0" && getSecondNumber === "0") {
+            return `${units[getFirstNumber]} ${hundreds[0]}`;
+        }
+        if (getLastNumber === "0" && getSecondNumber !== "1") {
+            return `${units[getFirstNumber]} ${hundreds[0]} ${
+                dozenth[getSecondNumber - 2]
+            }`;
+        }
+        if (getSecondNumber === "0") {
+            return `${units[getFirstNumber]} ${hundreds[0]} ${units[getLastNumber]}`;
+        }
+        if (getSecondNumber === "1") {
+            return `${units[getFirstNumber]} ${hundreds[0]} ${
+                units[Number(getLastNumber) + 10]
+            }`;
+        }
+        return `${units[getFirstNumber]} ${hundreds[0]} ${
+            dozenth[getSecondNumber - 2]
+        } ${units[getLastNumber]}`;
+    }
+};
